ae3178dbc908bf2a4ba55fb9c964768f14feaefa
sanjana-ravishankar <89596324+sanjana-ravishankar@users.noreply.github.com>
2023-06-22 16:14:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+import Typed from 'typed.js';
+
+let MenuBtn = document.getElementById('MenuBtn');
+MenuBtn.addEventListener('click',function(e) {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        this.classList.toggle('fa-xmark');
+})
+
+
+
+
+let typed = new Typed('auto-input' , {
+    strings:['Technical Writer','Graphic Designer','Creative Writer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+});
+
+let navLinks = document.querySelectorAll('nav ul li a')
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll',function() {
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && (scrollPos < section.offsetTop + section.offsetHeight)) {
+            navLinks.forEach(link => {
+                link.classList.remove('active')
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('active')
+                }
+                
+        });
+    }
+    });
+});
